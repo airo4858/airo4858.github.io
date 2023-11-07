1bcd55fd441a07091afbafacb8233ed44231fe00
Aidan Rogers <143754636+airo4858@users.noreply.github.com>
2023-11-07 10:30:28 -0700

    aewsxdrcfvghbjnkjnhhgbcdxsaedtgybhun

---
+++ ica/ica_week11/week11_lecture.js
@@ -0,0 +1,54 @@
+console.log("yo");
+
+const x = 2;
+const y = 2;
+
+if (x == y) {
+    console.log("YIPPIE");
+}
+else {
+    console.log("KILL YOURSELF");
+}
+
+const btn = document.querySelector("button");
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+document.addEventListener("DOMContentLoaded", () => {
+    canvas.width = document.documentElement.clientWidth;
+    canvas.height = document.documentElement.clientHeight;
+});
+
+function random(number) {
+    return Math.floor(Math.random() * number);
+}
+
+function draw() {
+    //console.log("AHHHHHHHHH");
+    //ctx.clearRect(0,0,canvas.width, canvas.height);
+    for (let i = 0; i < 100; i++){
+        if(i%2 == 0){
+            ctx.beginPath();
+            ctx.fillStyle = "rgba(0,255,0,0.5)";
+            ctx.arc (
+                random(canvas.width),
+                random(canvas.height),
+                random(100), 0, 2*Math.PI,
+            );
+            ctx.fill();
+        }
+        else {
+            ctx.beginPath();
+            ctx.fillStyle = "rgba(0,0,255,0.5)";
+            ctx.arc (
+                random(canvas.width),
+                random(canvas.height),
+                random(100), 0, 2*Math.PI,
+            );
+            ctx.fill();
+        }
+    }
+}
+
+btn.addEventListener("click", draw);
