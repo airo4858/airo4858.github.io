5da9c3013f60a3e7ba61f4c14b694b8ca25fa7b0
Aidan Rogers <143754636+airo4858@users.noreply.github.com>
2023-11-16 09:02:17 -0700

    why

---
+++ wa/wa12/wa12.js
@@ -0,0 +1,57 @@
+const newBtn = document.querySelector('#js-new-quote').addEventListener('click', getQuote);
+const oldBtn = document.querySelector('#js-tweet').addEventListener('click', getAnswer);
+const pressMe = document.querySelector('#js-press-me').addEventListener('click', doSomething);
+
+const endpoint ='https://trivia.cyberwisp.com/getrandomchristmasquestion';
+
+async function getQuote(){
+    //console.log('Test');
+
+    try {
+        const response = await fetch(endpoint);
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        const json = await response.json();
+        //console.log(json);
+        displayQuote(json['question']);
+    }
+    catch(err) {
+        console.log(err);
+        alert('Failed to fetch da thingie');
+    }
+}
+function displayQuote(question) {
+    const questionTxt = document.querySelector('#js-quote-text');
+    questionTxt.textContent = question;
+
+}
+
+async function getAnswer(){
+    //console.log('Test');
+
+    try {
+        const response = await fetch(endpoint);
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        const json = await response.json();
+        //console.log(json);
+        displayAnswer(json['answer']);
+    }
+    catch(err) {
+        console.log(err);
+        alert('Failed to fetch da thingie');
+    }
+}
+
+function displayAnswer(answer) {
+    const answerTxt = document.querySelector('#js-answer-text');
+    answerTxt.textContent = answer;
+
+}
+
+getQuote();
+function doSomething(){
+    displayQuote("I'm Tired");
+}
