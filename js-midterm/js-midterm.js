c1460caf36ab091a6b4c6e12b3f576b248ab97dc
Aidan Rogers <143754636+airo4858@users.noreply.github.com>
2023-12-12 13:02:10 -0700

    dsxfc njkml,

--- js-midterm/js-midterm.js
+++ js-midterm/js-midterm.js
@@ -4,6 +4,7 @@ function random(min, max) {
     return num;
   }
 
+const btn0 = document.querySelector('#b0').addEventListener('click', number0);
 const btn1 = document.querySelector('#b1').addEventListener('click', number1);
 const btn2 = document.querySelector('#b2').addEventListener('click', number2);
 const btn3 = document.querySelector('#b3').addEventListener('click', number3);
@@ -13,8 +14,17 @@ const btn6 = document.querySelector('#b6').addEventListener('click', number6);
 const btn7 = document.querySelector('#b7').addEventListener('click', number7);
 const btn8 = document.querySelector('#b8').addEventListener('click', number8);
 const btn9 = document.querySelector('#b9').addEventListener('click', number9);
+const reset = document.querySelector('#ob2').addEventListener('click',resetNumber);
+const submit = document.querySelector('#ob1').addEventListener('click', submitNumber);
 
 const newtext = document.querySelector('#phone-number');
+
+function number0() {
+    const n1 = 0;
+    let currentNumber = newtext.textContent;
+    let finalNum = n1 + currentNumber;
+    newtext.textContent = finalNum;
+}
 
 function number1() {
     const n1 = 9;
@@ -33,29 +43,57 @@ function number2() {
 function number3() {
     const n1 = 4;
     let currentNumber = newtext.textContent;
-    let finalNum = n1 + currentNumber;
-    newtext.textContent = finalNum;
+    if (currentNumber.length >= 5){
+        const n1 = 3;
+        let finalNum = n1 + currentNumber;
+        newtext.textContent = finalNum;
+    }
+    else {
+        let finalNum = n1 + currentNumber;
+        newtext.textContent = finalNum;
+    }
 }
 
 function number4() {
     const n1 = 3;
     let currentNumber = newtext.textContent;
-    let finalNum = n1 + currentNumber;
-    newtext.textContent = finalNum;
+    if (currentNumber.length >= 5){
+        const n1 = 4;
+        let finalNum = n1 + currentNumber;
+        newtext.textContent = finalNum;
+    }
+    else {
+        let finalNum = n1 + currentNumber;
+        newtext.textContent = finalNum;
+    }
 }
 
 function number5() {
     const n1 = 5;
     let currentNumber = newtext.textContent;
-    let finalNum = n1 + currentNumber;
-    newtext.textContent = finalNum;
+    if (currentNumber.length >= 5){
+        const n1 = 6;
+        let finalNum = n1 + currentNumber;
+        newtext.textContent = finalNum;
+    }
+    else {
+        let finalNum = n1 + currentNumber;
+        newtext.textContent = finalNum;
+    }
 }
 
 function number6() {
     const n1 = 6;
     let currentNumber = newtext.textContent;
-    let finalNum = n1 + currentNumber;
-    newtext.textContent = finalNum;
+    if (currentNumber.length >= 5){
+        const n1 = 5;
+        let finalNum = n1 + currentNumber;
+        newtext.textContent = finalNum;
+    }
+    else {
+        let finalNum = n1 + currentNumber;
+        newtext.textContent = finalNum;
+    }
 }
 
 function number7() {
@@ -77,4 +115,28 @@ function number9() {
     let currentNumber = newtext.textContent;
     let finalNum = n1 + currentNumber;
     newtext.textContent = finalNum;
+}
+
+function resetNumber() {
+    let finalNum = "";
+    newtext.textContent = finalNum;
+}
+
+function submitNumber() {
+    let currentNumber = newtext.textContent;
+    if (currentNumber == 0){
+        alert("Put in a number, STOOPID!");
+    }
+    else if (currentNumber.length > 10){
+        alert("too... many... number...");
+        resetNumber();
+    }
+    else if (currentNumber.length <= 9){
+        alert("your stupidty amazes me. You should be studied in a labratory.");
+        resetNumber();
+    }
+    else if (currentNumber.length == 10){
+        alert("WOWIE! BY GOLLY! YIPPIE! PHONE NUMEBR RECEIVED");
+        resetNumber();
+    }
 }
